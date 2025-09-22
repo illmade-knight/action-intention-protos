726451b127ee107eebaa50fa1e476e4fd13842c6
Tim Hawkins <56164332+illmade-knight@users.noreply.github.com>
2025-09-22 13:09:09 +0100

    fix: remove ts namespace

--- gen/ts/real/secure-envelope.ts
+++ gen/ts/real/secure-envelope.ts
@@ -11,26 +11,25 @@ export interface SecureEnvelope {
     signature: Uint8Array
 }
 
-export namespace S {
-    function toProto(envelope: SecureEnvelope): SecureEnvelopePb {
-        return create(SecureEnvelopePbSchema, {
-            senderId: envelope.senderId.toString(),
-            recipientId: envelope.recipientId.toString(),
-            messageId: envelope.messageId,
-            encryptedSymmetricKey: envelope.encryptedSymmetricKey,
-            encryptedData: envelope.encryptedData,
-            signature: envelope.signature,
-        });
-    }
-
-    function fromProto(protoEnvelope: SecureEnvelopePb): SecureEnvelope {
-        return {
-            senderId: URN.parse(protoEnvelope.senderId),
-            recipientId: URN.parse(protoEnvelope.recipientId),
-            messageId: protoEnvelope.messageId,
-            encryptedSymmetricKey: protoEnvelope.encryptedSymmetricKey,
-            encryptedData: protoEnvelope.encryptedData,
-            signature: protoEnvelope.signature,
-        };
-    }
+export function secureEnvelopeToProto(envelope: SecureEnvelope): SecureEnvelopePb {
+    return create(SecureEnvelopePbSchema, {
+        senderId: envelope.senderId.toString(),
+        recipientId: envelope.recipientId.toString(),
+        messageId: envelope.messageId,
+        encryptedSymmetricKey: envelope.encryptedSymmetricKey,
+        encryptedData: envelope.encryptedData,
+        signature: envelope.signature,
+    });
 }
+
+export function secureEnvelopeFromProto(protoEnvelope: SecureEnvelopePb): SecureEnvelope {
+    return {
+        senderId: URN.parse(protoEnvelope.senderId),
+        recipientId: URN.parse(protoEnvelope.recipientId),
+        messageId: protoEnvelope.messageId,
+        encryptedSymmetricKey: protoEnvelope.encryptedSymmetricKey,
+        encryptedData: protoEnvelope.encryptedData,
+        signature: protoEnvelope.signature,
+    };
+}
+
